ef2302b32cc814f8aa9eeb325577ccdc5cc59dd4
2006210 <2006210@K-H-2006210-L.Kfintech.com>
2025-01-21 12:38:39 +0530

    third commit

--- StringProg/src/in/kfintech/main/LaunchPalindrome.java
+++ StringProg/src/in/kfintech/main/LaunchPalindrome.java
@@ -3,7 +3,7 @@ public class LaunchPalindrome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s1="NITIN";
+		String s1="Tejas";
 		String s2="";
 		
 		for(int i=s1.length()-1;i>=0;i--)
