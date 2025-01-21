a9e168423d3530b97e647ce5725cc3e3dd1b5913
2006210 <2006210@K-H-2006210-L.Kfintech.com>
2025-01-21 12:17:31 +0530

    First Commit

---
+++ StringProg/src/in/kfintech/main/Testing.java
@@ -0,0 +1,10 @@
+package in.kfintech.main;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
